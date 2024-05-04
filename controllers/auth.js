c2ffe90c921ca0a4abf38fb9978ca69139696b6c
eflexTech <eflexmedia1@gmail.com>
2024-05-04 23:29:59 +0100

    G

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,7 +30,7 @@ exports.register = async (req, res, next)=>{
             email: req.body.email,
             fullName: req.body.fullName,
             phoneNumber: req.body.phoneNumber,
-            country: req.body.country,
+            userName: req.body.userName
          })
          const token = jwt.sign({id:newUser._id, isAdmin:newUser.isAdmin}, process.env.JWT, {expiresIn: "15m"})
          newUser.token = token
